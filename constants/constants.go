82790198718d4ecedc2446e89dc100adca3d95c4
prana <pranaytb777@gmail.com>
2025-12-20 14:10:46 +0000

    Learned about constants in the GO language

--- constants/constants.go
+++ constants/constants.go
@@ -1,6 +1,5 @@
 package main
 
-
 // Constants declared globally without explicitly declaring the datatype
 // The go compiler automatically infers the type.
 // These are called un-typed constants because we did not,
@@ -8,20 +7,18 @@ package main
 const PI = 3.14
 const GRAVITY = 9.81
 
-func main(){
+func main() {
 	// Constant declared locally with the explicitly naming the datatype.
 	// These are typed constants because we declared the variable type,
 	// explicitly.
 	const number int = 17
 
 	// Declaring multiple Constants.
-	const(
-		monday = 1
-		tuesday = 2
+	const (
+		monday    = 1
+		tuesday   = 2
 		wednesday = 4
-		thursday = "thursday"
+		thursday  = "thursday"
 	)
-
-
 
 }
